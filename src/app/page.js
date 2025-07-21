4a54eb14543f2ac87457d0466ed61e4d9746d711
Kaleeswari02 <kaleeskaleeswari02@gmail.com>
2025-07-21 13:57:32 +0530

    navbar created

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import { Header } from "./components/Header";
+
 import { Footer } from "./components/Footer";
+import Header from "./components/Header";
 import { HeroSection } from "./components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <p>Home</p>
-      <Header />
+      <Header/>
       <HeroSection />
       <Footer />
     </>
