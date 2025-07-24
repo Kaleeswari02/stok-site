7224780bd55f0b6b66503dac32b543489df081df
Kaleeswari02 <kaleeskaleeswari02@gmail.com>
2025-07-24 09:45:06 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:'export',
+     images: {
+    unoptimized: true, // ✅ disable next/image optimization for static export
+  },
+};
 
 export default nextConfig;
